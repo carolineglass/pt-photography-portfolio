bee73756201d82da6ec39337ff475868f7882347
Caroline <gypsychever@gmail.com>
2025-07-08 19:57:54 -0400

    initial commit Vite and Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-8">
+      <h1 className="text-3xl font-bold">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
