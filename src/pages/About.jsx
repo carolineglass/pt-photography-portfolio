c3451a4543f938abd292c6bf2fb8b17caafd2a6f
Caroline <gypsychever@gmail.com>
2025-09-08 16:22:36 -0400

    Add company logos grid to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
     <div>
       {/* About content */}
       <div className="grid md:grid-cols-2 gap-12 items-center mb-20">
-        <div>
+        <div className="order-2 md:order-1">
           <PortableText
             value={aboutData.content}
             components={{
@@ -31,10 +31,24 @@ const About = () => {
             }}
           />
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-center md:justify-end order-1 md:order-2">
           <img src={urlFor(aboutData.image).url()} alt={aboutData.imageAlt} />
         </div>
       </div>
+      {aboutData.companyLogos && aboutData.companyLogos.length > 0 && (
+        <div className="border-t pt-16">
+          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-8">
+            {aboutData.companyLogos.map((company, index) => (
+              <div key={index} className="flex items-center justify-center p-4">
+                <img
+                  src={urlFor(company.logo).fit("max").url()}
+                  alt={`${company.companyName} logo`}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
